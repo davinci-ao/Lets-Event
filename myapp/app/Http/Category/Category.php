386f18108ce475481b165f15423aca3119bf94e1
Omega <99021508@mydavinci.nl>
2018-06-27 14:48:10 +0200

    addded base category controller, model and web page

---
+++ myapp/app/Http/Category/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace App\Http\Category;
+    
+    /**
+     * Description of Category this is the model for the category
+     *
+     * @author peter
+     */
+    class Category extends \Illuminate\Database\Eloquent 
+    {
+	    
+    }
+    
---
+++ myapp/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use Illuminate\Http\Request;
+
+    /**
+     * Description of CategoryController
+     *
+     * @author Peter Verhaar
+     */
+    class CategoryController extends Controller {
+
+	    /**
+	     * 
+	     */
+	    public function index() 
+	    {
+		    return view('categoryPage');
+	    }
+	    
+	    /**
+	     * 
+	     */
+	    public function createCategory() 
+	    {
+		    
+	    }
+	    
+	     /**
+	     * 
+	     */
+	    public function editCategory() 
+	    {
+		    
+	    }
+	    
+	     /**
+	     * 
+	     */
+	    public function deleteCategory() 
+	    {
+		    
+	    }
+    }
+    
