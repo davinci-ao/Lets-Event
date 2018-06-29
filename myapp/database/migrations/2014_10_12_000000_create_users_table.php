0edac5c920c66fee5a808b7761e02cf36b4a0ca0
Peter Verhaar <99021508@mydavinci.nl>
2018-06-29 08:51:02 +0200

    Add files via upload

---
+++ myapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+    use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
+
+    class CreateUsersTable extends Migration {
+
+	    /**
+	     * Run the migrations.
+	     *
+	     * @return void
+	     */
+	    public function up() {
+		    Schema::create('users', function (Blueprint $table) {
+			    $table->increments('id');
+			    $table->string('student_nr');
+			    $table->string('firstname');
+			    $table->string('lastname');
+			    $table->integer('education_location_id');
+			    $table->string('email')->unique();
+			    $table->string('password');
+			    $table->enum('role', ['leerling', 'leeraar']);
+			    $table->rememberToken();
+			    $table->timestamps();
+		    });
+	    }
+
+	    /**
+	     * Reverse the migrations.
+	     *
+	     * @return void
+	     */
+	    public function down() {
+		    Schema::dropIfExists('users');
+	    }
+
+    }
+    
