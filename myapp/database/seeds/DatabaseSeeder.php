1565fafa9abf9066f60386eb8e911eee1923b6f3
Rawesh Chedi <99039154@mydavinci.nl>
2018-08-29 14:18:17 +0200

    database seeder aangemaakt om fake events aan te maken

--- myapp/database/seeds/DatabaseSeeder.php
+++ myapp/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+          DB::table('events')->insert([
+             'name' => str_random(10),
+             'category_id' => rand(),
+             'datum' => now(),
+             'time' => date('Y-m-d H:i:s'),
+             'price' => '10.00',
+        	 'location_id' => rand(),
+             'user_id' => rand(),
+           ]);
+
+    	// factory(App\Event::class, 50)->create();
+    	// er is geen app\event 
     }
 }
