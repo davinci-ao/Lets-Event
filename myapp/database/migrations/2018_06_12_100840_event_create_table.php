2a4918bd4a0a70bc60020a45a8228d4ef4216eec
Omega <99021508@mydavinci.nl>
2018-08-31 14:24:10 +0200

    no message

--- myapp/database/migrations/2018_06_12_100840_event_create_table.php
+++ myapp/database/migrations/2018_06_12_100840_event_create_table.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 use Illuminate\Support\Facades\Schema;
@@ -34,3 +35,41 @@ class EventCreateTable extends Migration
 		Schema::dropIfExists('events');
 	}
 }
+=======
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EventCreateTable extends Migration
+{
+  	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('events', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('category_id');
+			$table->date('datum');
+			$table->timestamp('time');
+			$table->double('price');
+			$table->integer('location_id');
+			$table->integer('user_id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('events');
+	}
+}
+>>>>>>> 69a981a70367703a5f65b37cde26dcbd2c48dd49
--- myapp/database/seeds/DatabaseSeeder.php
+++ myapp/database/seeds/DatabaseSeeder.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 use Illuminate\Database\Seeder;
@@ -14,3 +15,21 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
     }
 }
+=======
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+    }
+}
+>>>>>>> 69a981a70367703a5f65b37cde26dcbd2c48dd49
