0a259ba3e6ab0ef3fa4553c45a8be14c0289ade3
Peter Verhaar <99021508@mydavinci.nl>
2018-09-11 22:57:49 +0200

    updated, the myshop CSS  to LetsEvent CSS
     to load in the correct CSS

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/myshop.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/LetsEvent.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
