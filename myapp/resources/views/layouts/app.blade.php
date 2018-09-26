b103b69d596c475906755c5366167ae7ec811729
Bailey van Antwerpen <99041848@mydavinci.nl>
2018-09-26 11:03:23 +0200

    added 'Import CSV' to dropdown menu

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -53,6 +53,7 @@
 									<a class="dropdown-item" href="{{ route('home') }}" >Home</a>
 									<a class="dropdown-item" href="{{ route('indexCategory') }}" >View Categories</a>
 									<a class="dropdown-item" href="{{ route('eventIndex') }}" >View Events</a>
+									<a class="dropdown-item" href="{{ route('import') }}" >Import CSV</a>
 									<a class="dropdown-item" href="{{ route('logout') }}"
 									   onclick="event.preventDefault();
 										document.getElementById('logout-form').submit();">
