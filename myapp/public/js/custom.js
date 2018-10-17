f5d9e3ea7d3f0966ccd7761611c46302301009b1
Bailey van Antwerpen <99041848@mydavinci.nl>
2018-10-17 11:22:43 +0200

    added some small things to the tags front end

--- myapp/public/js/custom.js
+++ myapp/public/js/custom.js
@@ -94,8 +94,21 @@ $(document).ready(function () {
 	$('.multi-tag').select2({
 		placeholder: 'Select your category tags.',
 		tags: true,
+		closeOnSelect: false,
 		tokenSeparators: [' ', ',', ';'],
 		allowClear: true
+	});
+
+	$('.multi-tag').on('select2:closing', function () {
+		var tag = document.getElementsByClassName('select2-search__field')[0].value;
+		if (tag != '') {
+			var data = {
+				id: tag,
+				text: tag
+			};
+			var newOption = new Option(data.text, data.id, true, true);
+			$('.multi-tag').append(newOption).trigger('change');
+		}
 	});
 });
 
