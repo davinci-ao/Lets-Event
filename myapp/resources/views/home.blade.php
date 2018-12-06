d12173f81cfd22a475df290e2ddb538dffa5c032
Bailey van Antwerpen <99041848@mydavinci.nl>
2018-12-06 13:04:19 +0100

    peter, dont break it again ok

--- myapp/resources/views/home.blade.php
+++ myapp/resources/views/home.blade.php
@@ -40,15 +40,15 @@
 						<div class="card">
 							<div class="card-header">Your personal data</div>
 							<div class="card-body">
-								<p>Firstname: {{$user->firstname}}</p>
-								<p>Lastname: {{$user->lastname}}</p>
-								<p>Email: {{$user->email}}</p>
-								<p>Role: {{$user->role}}</p>
-								<p>Education location: {{$location->name}}</p>
+								<p>Firstname: {{ $user->firstname }}</p>
+								<p>Lastname: {{ $user->lastname }}</p>
+								<p>Email: {{ $user->email }}</p>
+								<p>Role: {{ $user->role }}</p>
+								<p>Education location: {{ $location->name}}</p>
 								@if ($user->role == 'student')
-								<p>Student number: {{$user->student_nr}}</p>
+								<p>Student number: {{ $user->student_nr }}</p>
 								@elseif ($user->role == 'teacher')
-								<p>Teacher code: {{$user->student_nr}}</p>
+								<p>Teacher code: {{ $user->student_nr }}</p>
 								@endif
 							</div>
 						</div>
